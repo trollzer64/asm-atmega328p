4540148b2a6231f6d02dc0d4249641103301eefd
trollzer64 <trollzer64@gmail.com>
2020-06-15 23:01:39 -0300

    Working clock

--- display-clock/display-clock/main.asm
+++ display-clock/display-clock/main.asm
@@ -4,13 +4,13 @@
 ; Created: 15/06/2020 21:48:10
 ;
 
-.equ B0 = PC0				; Bot„o 0
-.def AUX = R16				; Para principais configuraÁıes
+.def AUX = R16				; Para principais configura√ß√µes
 .def CTRL = R17				; LCD: E, RW, RS,-,-,-,-,-
 .def DATA = R18				; LCD: D[0..7]
-.def M = R19
-.def S = R20
-.def MAXV = R21
+.def H = R19				; Registrador para guardar horas (Um d√≠gito por nibble: [D0,D1])
+.def M = R20				; Registrador para guardar minutos (Um d√≠gito por nibble: [D0,D1])
+.def S = R21				; Registrador para guardar segundos (Um d√≠gito por nibble: [D0,D1])
+.def MAXV = R22				; Registrador para o n√∫mero m√°ximo de um d√≠gito
 
 .ORG 0x0000
 RJMP reset
@@ -18,120 +18,150 @@ RJMP reset
 RJMP timer2_ovf
 
 reset:
-	; Botao
-	LDI AUX,0b11111110
-	OUT DDRC,AUX
-	LDI AUX,0b11000001
-	OUT PORTC,AUX
-	; Timer
-	LDI AUX,0b00100000	; (AS2)
-	STS ASSR,AUX
-	LDI AUX,0b00000101	; (CS22|CS20)
-	STS TCCR2B,AUX
-	; InterrupÁıes
-	SEI
-	LDI AUX,0b00000001	; (TOIE2)
-	STS TIMSK2,AUX		; InterrupÁ„o do Timer/Counter 2
 	; LCD data
 	LDI AUX,0b11111111	; D[0..7]
 	OUT DDRD,AUX
 	; LCD control
-	LDI AUX,0b00000111	; E, RW, RS
+	LDI AUX,0b00000111	; E, RW, RS,-,-,-,-,-
 	OUT DDRB,AUX
-	; Initial time
+	; Tempo inicial
+	LDI H,0x23
 	LDI M,0x59
 	LDI S,0x45
-; INICIALIZA«√O DO DISPLAY (TABELA 13)
-init:
-	; 1 - Power supply
-	RCALL wait_fcn
-	; 2 - Function set
-	LDI CTRL,0b00000000
-	LDI DATA,0b00111000		; 8b mode; 2-line display; 5x8 font
-	RCALL send_fcn
-	; 3 - Display on/off control
-	LDI CTRL,0b00000000
-	LDI DATA,0b00001100		; Display on; Cursor off; blink off
-	RCALL send_fcn
-	; 4 - Entry mode set :
-	LDI CTRL,0b00000000
-	LDI DATA,0b00000110		; Increment and shift cursor; Don't shift display
-	RCALL send_fcn
-
+	; INICIALIZA√á√ÉO DO DISPLAY
+	lcd_init:
+		; 1 - Power supply
+		RCALL wait_fcn
+		; 2 - Function set
+		LDI CTRL,0b00000000
+		LDI DATA,0b00111000		; 8b mode; 2-line display; 5x8 font
+		RCALL send_fcn
+		; 3 - Display on/off control
+		LDI CTRL,0b00000000
+		LDI DATA,0b00001100		; Display on; Cursor off; blink off
+		RCALL send_fcn
+		; 4 - Entry mode set :
+		LDI CTRL,0b00000000
+		LDI DATA,0b00000110		; Increment and shift cursor; Don't shift display
+		RCALL send_fcn
+	
+	; Setup Timer - Oscilador externo, modo ass√≠ncrono
+	LDI AUX,0b00100000	; (AS2) Asynchronous Timer/Counter 2
+	STS ASSR,AUX
+	LDI AUX,0b00000101	; (CS22|CS20) prescaler = 128
+	STS TCCR2B,AUX
+	; Interrup√ß√µes - Timer 2, modo overflow
+	SEI
+	LDI AUX,0b00000001	; (TOIE2) Overflow Interrupt Enable
+	STS TIMSK2,AUX
+; LOOP PARA INTERROMPER
 loop:
 	NOP
 	RJMP loop
-
+; INCREMENTO DE TEMPO E DISPLAY DO NOVO D√çGITO
 timer2_ovf:
-	; incrementar aqui M ou S
-	
 	; S(0)
 	LDI MAXV,0x09
-	MOV AUX,S			; Carrega n˙mero do nibble
+	MOV AUX,S			; Carrega n√∫mero do nibble
 	ANDI AUX,0x0F
-	RCALL limit_fcn		; Incrementa e limita ao numero m·ximo
+	RCALL limit_fcn		; Incrementa e limita ao numero m√°ximo
 	ANDI S,0xF0
-	ADD S,AUX			; Guarda n˙mero no nibble
+	ADD S,AUX			; Guarda n√∫mero no nibble
 	CPI AUX,0x00
-	BRNE send_message	; Se overflow n˙mero, incrementa o prÛximo
+	BRNE send_message	; Se overflow nÔøΩmero, incrementa o prÔøΩximo
 	
 	; S(1)
 	LDI MAXV,0x05
-	MOV AUX,S			; Carrega n˙mero do nibble
+	MOV AUX,S			; Carrega nÔøΩmero do nibble
 	ANDI AUX,0xF0
 	SWAP AUX
-	RCALL limit_fcn		; Incrementa e limita ao numero m·ximo
+	RCALL limit_fcn		; Incrementa e limita ao numero mÔøΩximo
 	SWAP AUX
 	ANDI S,0x0F
-	ADD S,AUX			; Guarda n˙mero no nibble
+	ADD S,AUX			; Guarda nÔøΩmero no nibble
 	CPI AUX,0x00
-	BRNE send_message	; Se overflow n˙mero, incrementa o prÛximo
+	BRNE send_message	; Se overflow nÔøΩmero, incrementa o prÔøΩximo
 	
 	; M(0)
 	LDI MAXV,0x09
-	MOV AUX,M			; Carrega n˙mero do nibble
+	MOV AUX,M			; Carrega nÔøΩmero do nibble
 	ANDI AUX,0x0F
-	RCALL limit_fcn		; Incrementa e limita ao numero m·ximo
+	RCALL limit_fcn		; Incrementa e limita ao numero mÔøΩximo
 	ANDI M,0xF0
-	ADD M,AUX			; Guarda n˙mero no nibble
+	ADD M,AUX			; Guarda nÔøΩmero no nibble
 	CPI AUX,0x00
-	BRNE send_message	; Se overflow n˙mero, incrementa o prÛximo
+	BRNE send_message	; Se overflow nÔøΩmero, incrementa o prÔøΩximo
 	
 	; M(1)
 	LDI MAXV,0x05
-	MOV AUX,M			; Carrega n˙mero do nibble
+	MOV AUX,M			; Carrega nÔøΩmero do nibble
 	ANDI AUX,0xF0
 	SWAP AUX
-	RCALL limit_fcn		; Incrementa e limita ao numero m·ximo
+	RCALL limit_fcn		; Incrementa e limita ao numero mÔøΩximo
 	SWAP AUX
 	ANDI M,0x0F
-	ADD M,AUX			; Guarda n˙mero no nibble
+	ADD M,AUX			; Guarda nÔøΩmero no nibble
 	CPI AUX,0x00
-	BRNE send_message	; Se overflow n˙mero, incrementa o prÛximo
-send_message:
-	RCALL write
+	BRNE send_message	; Se overflow nÔøΩmero, incrementa o prÔøΩximo
+
+	; H(0)
+	LDI MAXV,0x03
+	MOV AUX,H			; Carrega nÔøΩmero do nibble
+	ANDI AUX,0x0F
+	RCALL limit_fcn		; Incrementa e limita ao numero mÔøΩximo
+	ANDI H,0xF0
+	ADD H,AUX			; Guarda nÔøΩmero no nibble
+	CPI AUX,0x00
+	BRNE send_message	; Se overflow nÔøΩmero, incrementa o prÔøΩximo
+	
+	; H(1)
+	LDI MAXV,0x02
+	MOV AUX,H			; Carrega nÔøΩmero do nibble
+	ANDI AUX,0xF0
+	SWAP AUX
+	RCALL limit_fcn		; Incrementa e limita ao numero mÔøΩximo
+	SWAP AUX
+	ANDI H,0x0F
+	ADD H,AUX			; Guarda nÔøΩmero no nibble
+	CPI AUX,0x00
+	BRNE send_message	; Se overflow nÔøΩmero, incrementa o prÔøΩximo
+	send_message:
+		RCALL write_fcn
 	RETI
 
-; limit_fcn(AUX, MAXV)
+; limit_fcn(AUX, MAXV) - Incrementa um valor com um limite m√°ximo
 limit_fcn:
-	CP AUX,MAXV
+	CP AUX,MAXV			; Se n√∫mero m√°ximo...
 	BRNE incr
-	LDI AUX,0xFF
-incr:
-	INC AUX
-
+	LDI AUX,0xFF		; Zerar valor
+	incr:
+		INC AUX
 	RET
 
-; ESCREVENDO CARACTERES
-write:
+; write_fcn(H, M, S) - Entra no display o valor selecionado
+write_fcn:
 	; Clear display :
 	LDI CTRL,0b00000000
-	LDI DATA,0b00000001		; Clear display
+	LDI DATA,0b00000001		; Clear display command
 	RCALL send_fcn
 	
 	; 5 - Write data to CGRAM/DDRAM :
 	LDI CTRL,0b00000100
-	LDI AUX,'0'
+	LDI AUX,'0'				; Valor base do char '0'
+
+	MOV DATA,H
+	ANDI DATA,0xF0
+	SWAP DATA
+	ADD DATA,AUX
+	RCALL send_fcn
+
+	MOV DATA,H
+	ANDI DATA,0x0F
+	ADD DATA,AUX
+	RCALL send_fcn
+
+	LDI DATA,':'
+	RCALL send_fcn
 
 	MOV DATA,M
 	ANDI DATA,0xF0
@@ -160,7 +190,7 @@ write:
 
 	RET
 
-; (CTRL, DATA)
+; send_fcn(CTRL, DATA) - envia um byte de dados ao LCD
 send_fcn:
 	RCALL wait_fcn			; Check busy flag
 
@@ -174,59 +204,52 @@ send_fcn:
 
 	RET
 
+; wait_fcn() - Espera a busy flag do LCD estar livre
 wait_fcn:
 	PUSH R16
 	PUSH R17
 	
 	; PORTD input
-	LDI R16,0b0000000	; D[0..7]
+	LDI R16,0b00000000			; D[0..7]
 	OUT DDRD,R16
-busy:
-	; Send read command
-	LDI R16,0b00000010	; E, RW, RS
-	OUT PORTB,R16
-	ORI R16,0b00000001		; E on 1
-	OUT PORTB,R16
-	; Check busy flag
-	IN R17,PORTD
-	SBRC R17,7
-	RJMP busy
+	busy:
+		; Send read command
+		LDI R16,0b00000010		; E, RW, RS
+		OUT PORTB,R16
+		ORI R16,0b00000001		; E on 1
+		OUT PORTB,R16
+		; Check busy flag
+		IN R17,PORTD
+		SBRC R17,7
+		RJMP busy
 
-	RCALL delay
+	RCALL delay_fcn				; Espera de 0.000006s necess√°ria‚Ä¨
 
 	; End of LCD instruction
-	ANDI R16,0b11111110	; E on 0
+	ANDI R16,0b11111110			; E on 0
 	OUT PORTB,R16
 
 	; PORTD output
-	LDI R16,0b1111111	; D[0..7]
+	LDI R16,0b11111111			; D[0..7]
 	OUT DDRD,R16
 
 	POP R17
 	POP R16
 	RET
 
-delay:
+; delay_fcn() -  Delay necess√°rio ap√≥s clear da busy flag do LCD
+delay_fcn:
 	PUSH R18
 	PUSH R17
 	LDI R17,0x02
 	LDI R18,0xFF
 	; t = 0.000071s, t_ADD = 0.000040s
-back:
-	DEC R17			; 1		CLK
-	BRNE back		; 1/2	CLK
-	DEC R18			; 1		CLK
-	BRNE back		; 1/2	CLK
-
+	back:
+		DEC R17			; 1		CLK
+		BRNE back		; 1/2	CLK
+		DEC R18			; 1		CLK
+		BRNE back		; 1/2	CLK
 	POP R17
 	POP R18
 	RET
 
-man_break:
-	SBIC PINC,B0
-	RJMP man_break
-latch:
-	SBIS PINC,B0
-	RJMP latch
-
-	RET
